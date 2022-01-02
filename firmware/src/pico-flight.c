407f68332c36562bdf7539be6bce5dc73c6ce4ab
SpikE4343 <johnrohrssen@gmail.com>
2022-01-01 21:19:31 -0800

    Update to kicad 6 more progress on 20x20 board layout

--- firmware/src/pico-flight.c
+++ firmware/src/pico-flight.c
@@ -3,17 +3,17 @@
 #include "flight_controller.h"
 #include "data_vars.h"
 
-#define USING_PICO_PROBE 0
+#define USING_PICO_PROBE 1
 
 int main()
 {
   stdio_init_all();
 
-// #if USING_PICO_PROBE
-//   gpio_set_function(16, GPIO_FUNC_UART);
-//   gpio_set_function(17, GPIO_FUNC_UART);
-//   uart_set_fifo_enabled(uart0, true);
-// #endif
+#if USING_PICO_PROBE
+  gpio_set_function(16, GPIO_FUNC_UART);
+  gpio_set_function(17, GPIO_FUNC_UART);
+  uart_set_fifo_enabled(uart0, true);
+#endif
 
   telemetry_init();
   data_vars_init();
